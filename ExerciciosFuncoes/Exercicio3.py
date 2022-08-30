9fc384b1194e8960c7ff442cfef9cd2f24cee601
Victor Mendes <vhs.mendes18@gmail.com>
2022-08-30 19:31:31 -0300

    Create Exercicio3.py

---
+++ ExerciciosFuncoes/Exercicio3.py
@@ -0,0 +1,13 @@
+"""
+Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+"""
+
+
+def somar(n1, n2, n3):
+    return n1 + n2 + n3
+
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+n3 = int(input('Digite um valor: '))
+print(somar(n1, n2, n3))
