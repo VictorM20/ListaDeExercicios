ba8a1ac8e9ccd394a188026b48250f1dddcf2024
Victor Mendes <vhs.mendes18@gmail.com>
2022-09-15 19:34:14 -0300

    Update Exercicio23.py

--- ExerciciosListas/Exercicio23.py
+++ ExerciciosListas/Exercicio23.py
@@ -29,18 +29,19 @@ de forma a agilizar a execução do programa. A conversão da espaço ocupado em
  feita através de uma função separada, que será chamada pelo programa principal.
  O cálculo do percentual de uso também deverá ser feito através de uma função, que será chamada pelo programa principal.
 """
-with open("usuarios.txt", "r") as arquivo:
-    usuarios = arquivo.readlines()
-    usuarios = [usuario.strip().split() for usuario in usuarios]
-    usuarios = [[usuario[0], int(usuario[1])] for usuario in usuarios]
-    usuarios = sorted(usuarios, key=lambda usuario: usuario[1], reverse=True)
-    total = sum([usuario[1] for usuario in usuarios])
-    media = total / len(usuarios)
-    with open("relatorio.txt", "w") as relatorio:
-        relatorio.write("ACME Inc.               Uso do espaço em disco pelos usuários\n")
-        relatorio.write("-" * 80 + "\n")
-        relatorio.write("Nr.  Usuário        Espaço utilizado     % do uso\n\n")
-        for i, usuario in enumerate(usuarios):
-            relatorio.write(f"{i + 1:<4} {usuario[0]:<15} {usuario[1] / 1024 / 1024:10.2f} MB {usuario[1] / total * 100:10.2f}%\n")
-        relatorio.write(f"\nEspaço total ocupado: {total / 1024 / 1024:10.2f} MB\n")
-        relatorio.write(f"Espaço médio ocupado: {media / 1024 / 1024:10.2f} MB\n")
+with open("usuarios.txt", "r") as arquivo:  # Abrindo o arquivo
+    usuarios = arquivo.readlines()  # Lendo o arquivo
+    usuarios = [usuario.strip().split() for usuario in usuarios]  # Separando os dados
+    usuarios = [[usuario[0], int(usuario[1])] for usuario in usuarios]  # Transformando o espaço em inteiro
+    usuarios = sorted(usuarios, key=lambda usuario: usuario[1], reverse=True)  # Ordenando os dados
+    total = sum([usuario[1] for usuario in usuarios])  # Somando o total de espaço
+    media = total / len(usuarios)  # Calculando a média
+    with open("relatorio.txt", "w") as relatorio:  # Abrindo o arquivo de relatório
+        relatorio.write("ACME Inc.               Uso do espaço em disco pelos usuários\n")  # Escrevendo o cabeçalho
+        relatorio.write("-" * 80 + "\n")  # Escrevendo o cabeçalho
+        relatorio.write("Nr.  Usuário        Espaço utilizado     % do uso\n\n")  # Escrevendo o cabeçalho
+        for i, usuario in enumerate(usuarios):  # Percorrendo os usuários
+            relatorio.write(
+                f"{i + 1:<4} {usuario[0]:<15} {usuario[1] / 1024 / 1024:10.2f} MB {usuario[1] / total * 100:10.2f}%\n")  # Escrevendo os dados
+        relatorio.write(f"\nEspaço total ocupado: {total / 1024 / 1024:10.2f} MB\n")  # Escrevendo o total
+        relatorio.write(f"Espaço médio ocupado: {media / 1024 / 1024:10.2f} MB\n")  # Escrevendo a média
