cc8a2a43a41dcb71e6aa3f5d3e4544ad565d9d6b
Victor Mendes <vhs.mendes18@gmail.com>
2022-09-16 20:19:19 -0300

    Update 01.py

--- EstruturadeRepeticao/01.py
+++ EstruturadeRepeticao/01.py
@@ -2,8 +2,8 @@
 Faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 """
-
-nota = int(input('Digite uma nota entre [0 e 10]: '))
-while (nota < 0 or nota > 10):
-    nota = int(input('Nota inválida. Digite uma nota entre [0 e 10]: '))
-    continue
+n = int(input("Digite uma nota entre 0 e 10: "))
+while n < 0 or n > 10:
+    print("Nota inválida!")
+    n = int(input("Digite uma nota entre 0 e 10: "))
+print("Nota válida!")
