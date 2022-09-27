2725649591ec792db8ec40187803c3d36a78243e
Victor Mendes <vhs.mendes18@gmail.com>
2022-09-27 20:57:58 -0300

    Create Exercicio1.py

---
+++ ExerciciosClasses/Exercicio1.py
@@ -0,0 +1,20 @@
+"""
+Classe Bola: Crie uma classe que modele uma bola:
+
+Atributos: Cor, circunferência, material
+Métodos: trocaCor e mostraCor
+
+"""
+
+
+class Bola:
+    def __init__(self, cor, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+
+    def trocaCor(self, cor):
+        self.cor = cor
+
+    def mostraCor(self):
+        return self.cor
