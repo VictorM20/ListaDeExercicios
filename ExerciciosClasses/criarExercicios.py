b7fd0f7285b301db7530f03319fab707e53e4a95
Victor Mendes <vhs.mendes18@gmail.com>
2022-10-10 10:19:44 -0300

    Create criarExercicios.py

---
+++ ExerciciosClasses/criarExercicios.py
@@ -0,0 +1,14 @@
+"""
+Criar arquivos.py com nome Exercicios9 até Exercicios17
+
+"""
+
+def criarArquivos():
+    for i in range(9, 18):
+        nome = f"Exercicios{i}.py"
+        arquivo = open(nome, 'w')
+        arquivo.close()
+
+criarArquivos()
+
+
