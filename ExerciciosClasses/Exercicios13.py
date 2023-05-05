dfc80f4179ab042d3799a935b8f2c47bec962fae
Victor Mendes <vhs.mendes18@gmail.com>
2023-05-04 21:19:13 -0300

    Exercicio 13

--- ExerciciosClasses/Exercicios13.py
+++ ExerciciosClasses/Exercicios13.py
@@ -2,3 +2,17 @@
 Classe Funcionário: Implemente a classe Funcionário. Um empregado tem um nome (um string) e um salário(um double).
 Escreva um construtor com dois parâmetros (nome e salário) e métodos para devolver nome e salário. Escreva um pequeno programa que teste sua classe.
 """
+class Funcionario:
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+
+    def getNome(self):
+        return self.nome
+
+    def getSalario(self):
+        return self.salario
+
+funcionario1 = Funcionario("João", 2500.00)
+print(funcionario1.getNome())
+print(funcionario1.getSalario())
