d1577a37bf06f76e3cab94da52703645eabb9eac
Victor Mendes <vhs.mendes18@gmail.com>
2023-05-04 21:19:50 -0300

    Exercicio 14

--- ExerciciosClasses/Exercicios14.py
+++ ExerciciosClasses/Exercicios14.py
@@ -4,3 +4,22 @@ Exemplo de uso:
   harry=funcionário("Harry",25000)
   harry.aumentarSalario(10)
 """
+class Funcionario:
+    def __init__(self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+
+    def get_nome(self):
+        return self.nome
+
+    def get_salario(self):
+        return self.salario
+
+    def aumentarSalario(self, porcentualDeAumento):
+        aumento = self.salario * (porcentualDeAumento / 100)
+        self.salario += aumento
+
+harry = Funcionario("Harry", 25000)
+print(harry.get_nome(), harry.get_salario()) # Harry 25000
+harry.aumentarSalario(10)
+print(harry.get_nome(), harry.get_salario()) # Harry 27500.0
