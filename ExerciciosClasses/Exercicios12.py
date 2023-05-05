a0e5e796d6c9a74427e5e05aad38422001a1038a
Victor Mendes <vhs.mendes18@gmail.com>
2023-05-04 21:18:30 -0300

    Exercicio 12

--- ExerciciosClasses/Exercicios12.py
+++ ExerciciosClasses/Exercicios12.py
@@ -5,3 +5,21 @@ Forneça um método adicioneJuros (sem parâmetro explícito) que adicione juros
 Escreva um programa que construa uma poupança com um saldo inicial de R$1000,00 e uma taxa de juros de 10%.
 Depois aplique o método adicioneJuros() cinco vezes e imprime o saldo resultante.
 """
+class contaInvestimento:
+    def __init__(self, saldo_inicial, taxa_juros):
+        self.saldo = saldo_inicial
+        self.taxa_juros = taxa_juros
+
+    def adicioneJuros(self):
+        juros = self.saldo * (self.taxa_juros / 100)
+        self.saldo += juros
+
+# criando uma conta de investimento com saldo inicial de R$1000,00 e taxa de juros de 10%
+poupanca = contaInvestimento(1000, 10)
+
+# adicionando juros 5 vezes
+for i in range(5):
+    poupanca.adicioneJuros()
+
+# imprimindo o saldo resultante
+print("Saldo da poupança após 5 meses de juros: R$%.2f" % poupanca.saldo)
